a06108444d31035b1c4f2212be2e7e4bf07849b0
Mohamed el Habib <mohamedelhabib.benouis@hotmail.com>
2025-10-15 01:34:55 +0200

    fixing scanf to getline for mem allocation (str)

--- sequence_analyser/HUMMING_utils.c
+++ sequence_analyser/HUMMING_utils.c
@@ -7,9 +7,13 @@ void search_for_sequence_Humming(char *sequence)
     long long int j = 0,humming_value = 0;
     long long int seq_len = strlen(sequence);
     char *target =  NULL;
+    long long int target_len = 0;
+    int ret;
+
     printf("Insert the Target sequence\n");
-    scanf("%s", target);
-    long long int target_len = strlen(target);
+    ret = getline(&target, &target_len,stdin);
+    if(ret == -1)
+        printf("Get line error Target sequence \n");
     printf("Insert the  Humming Value\n");
     scanf("%lli", &humming_value);
 
@@ -29,9 +33,11 @@ void search_for_sequence_Humming(char *sequence)
         if (j >= target_len - humming_value)
         {
             printf("First Occurence of target found in Sequence at position %lli and Humming value of %lli",i,target_len - j);
+            free(target);
             return;
         }
         i++;
     }
+    free(target);
     printf("Target not found in sequence using with Humming Value of %lli",humming_value);
 }
